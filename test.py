bfff625ce4fa396daf3812ce4fb0e6b973897a5e
themoog <nbernard@jumpstartbroadcast.com>
2018-05-14 21:23:18 +0100

    another update

--- test.py
+++ test.py
@@ -1,30 +1,10 @@
 from easysnmp import Session
 import uuid
 import sqlite3
+import sys
 
-
-
-
-def main():
-
-
-	
-	#conn = sqlite3.connect('test.db')
-	#print "Opened database successfully";
-
-	print "Main Function"
-
-	#getSnmpTrap()
-	#insertIntoDatabase()
-	printTest()
-
-
-if __name__ == "__main__":
-	main()
-
-
-
-
+def printTest():
+	print "testprinttest"
 
 def getSnmpTrap():
 
@@ -32,11 +12,10 @@ def getSnmpTrap():
 	session = Session(hostname='127.0.0.1', community='public', version=2)
 
 	snmpTag1 = ".1.3.6.1.2.1.1.1.0"
-	description = session.get(snmpTag1)
-	result = str(description)
-	description2 = result.split("'")
-	print description2[1]	
+	
+	result = str(session.get(snmpTag1)).split("'")
 
+	return result[1]
 
 
 def insertIntoDatabase():
@@ -47,12 +26,34 @@ def insertIntoDatabase():
 
 
 	conn.execute("INSERT INTO RESULTS (ID,UUID,DATA) \
-		VALUES (?,?,?);", (str(uuid.uuid4()),"23","14"))
+		VALUES (?,?,?);", (str(uuid.uuid4()),getSnmpTrap(),"14"))
 
 	conn.commit()
 	print "Records created successfully";
 	conn.close()
 
-def printTest():
-	print "testprinttest"
+
+def main():
+
+
+	
+	#conn = sqlite3.connect('test.db')
+	#print "Opened database successfully";
+
+
+	print getSnmpTrap()
+	insertIntoDatabase()
+
+
+if __name__ == "__main__":
+	main()
+
+
+
+
+
+
+
+
+
 	
